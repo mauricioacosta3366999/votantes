eeb4225eebf522779ba50f9920ce7b29875b4185
Mauricio Acosta <acostamauricio336699@gmail.com>
2022-12-14 15:17:55 -0300

    crear miembros

---
+++ lib/pages/agregarMiembro.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:votantes/appConfig.dart';
+import 'package:votantes/pages/widgets/appbar.dart';
+import 'package:votantes/pages/widgets/backImage.dart';
+import 'package:votantes/pages/widgets/prymaryButton.dart';
+import 'package:votantes/pages/widgets/textField.dart';
+import 'package:votantes/services/endpoints.dart';
+
+class AgregarMiembro extends StatefulWidget {
+  const AgregarMiembro({super.key});
+  @override
+  State<AgregarMiembro> createState() => _AgregarMiembroState();
+}
+
+class _AgregarMiembroState extends State<AgregarMiembro> {
+  final cdiController = TextEditingController();
+  final passController = TextEditingController();
+  bool loading = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          const PreferredSize(preferredSize: Size(0, 60), child: MyAppbar()),
+      body: Stack(
+        children: [
+          const BackgroundImage(),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.1),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Agregar Miembro',
+                    style: AppCongig().prymaryStrongTextStyle,
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+                  Text(
+                    'Creá los credenciales',
+                    style: AppCongig().prymaryTextStyle,
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                  MyTextField(
+                      icon: const Icon(
+                        Icons.chrome_reader_mode_rounded,
+                        color: Colors.white,
+                      ),
+                      controller: cdiController,
+                      labelText: "Número de CDI"),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+                  MyTextField(
+                    icon: const Icon(
+                      Icons.key,
+                      color: Colors.white,
+                    ),
+                    controller: passController,
+                    labelText: 'Contraseña',
+                    isPass: true,
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                  loading
+                      ? const CircularProgressIndicator()
+                      : PrymaryButton(
+                          text: 'Crear miembro',
+                          function: () async {
+                            setState(() => loading = true);
+                            var res = await Endpoints()
+                                .memberCreate(ci: '123123', pass: '123123');
+                            setState(() => loading = false);
+                          },
+                        )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
